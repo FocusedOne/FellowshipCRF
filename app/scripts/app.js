aa25b6b0853ea940c1e5b8158806d28a73083cfa
allenjprice <allenjprice@gmail.com>
2014-06-03 18:46:19 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var app = angular.module('fellowshipCrfApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/fullForm.html',
+        controller: 'FormCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/form.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fellowshipCrfApp')
+  .controller('FormCtrl', function ($scope) {
+  
+  });
