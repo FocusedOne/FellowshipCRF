47e796bc66dd91e26b0083e37fd5d79c7bac1864
allenjprice <allenjprice@gmail.com>
2014-06-04 11:46:52 -0500

    Wiring up two-way binding between view and controller for everything except selects and checkboxes.

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -5,13 +5,70 @@ app.controller('FormCtrl', function($scope){
 
   
   $scope.form = {
+    //ministry info
     ministryEventName: '',
     ministryChoice: '',
     ministryAccount: '',
     ministryProjectContact: '',
     ministryProjectContactPhone: '',
     ministryOverseer: '',
-    ministryOverseerPhone: ''
+    ministryOverseerPhone: '',
+
+    //campusWidePromotion - checkboxes?
+
+    //bulletin announcement
+    bulletinSpotlightCopy: '',
+    bulletinSpotlightDates: '',
+    bulletinComingSoonCopy: '',
+    bulletinComingSoonDates: '',
+    bulletinThisWeekCopy: '',
+    bulletinThisWeekDates: '',
+
+    //tmaf announcement
+    tmafCopy: '',
+    tmafMonth: '',
+
+    //connection center booth
+    connectionCenterPerson: '',
+    connectionCenterItems: '',
+    connectionCenterDates: '',
+
+    //print media
+    printRequest: '',
+    printProjectInfo: '',
+    printLogoDates: '',
+    printPostersQuantity: '',
+    printPostersDates: '',
+    printFlyersQuantity: '',
+    printFlyersDates: '',
+    printBrochuresQuantity: '',
+    printBrochuresDates: '',
+    printNotebooksQuantity: '',
+    printNotebooksDates: '',
+    printOtherQuantity: '',
+    printOtherDates: '',
+    printOtherDescription: '',
+
+    //website
+      //websiteSite selection?
+    websiteWebpage: '',
+    websiteCopyDates: '',
+    websiteCopyTest: '',
+      //website gfx type
+    websiteGfxDates: '',
+    websiteGfxDescription: '',
+
+    //social media
+      //social campus selection
+    socialDates: '',
+    socialCopy: '',
+
+    //email blast
+      //email campus selection
+    emailList: '',
+    emailDates: '',
+    emailCopy: '',
+      //gfx/vid embed?
   };
   console.log('FormCtrl loaded');
 });
