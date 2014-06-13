d78c2d95c1cd266d84fe3c26792186c896a7ec97
allenjprice <allenjprice@gmail.com>
2014-06-13 11:01:08 -0500

    Oops.

--- app/scripts/keys.js
+++
@@ -1,4 +0,0 @@
-/* global PARSE_APP_ID: true, PARSE_REST_KEY: true */
-'use strict';
-var PARSE_APP_ID = 'uOblcxMCwRdLlItk3JV8U4jPIIVeKzdEIPiK4Teh';
-var PARSE_REST_KEY = 'tMta82HctohzeMkdnuKSONbZX1JXkbdHnQ3KP9AI';
