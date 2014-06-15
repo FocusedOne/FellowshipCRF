a581fab664867d910e89c6ec0b74175ab4e03363
allenjprice <allenjprice@gmail.com>
2014-06-15 14:23:11 -0500

    Reformatting $http requests for easier reading

--- app/scripts/services/crfData.js
+++ app/scripts/services/crfData.js
@@ -5,22 +5,57 @@ app.factory('crfData', function($http, $location){
     apiPath: 'https://api.parse.com/1/classes/crf',
     currentObject: '',
     save: function(formData){
-      return $http({method: 'POST', url: this.apiPath, headers: {'X-Parse-Application-Id': PARSE_APP_ID, 'X-Parse-REST-API-Key': PARSE_REST_KEY, 'Content-Type':'application/json'}, data: formData})
+      return $http({
+        method: 'POST',
+        url: this.apiPath,
+        headers: {
+          'X-Parse-Application-Id': PARSE_APP_ID,
+          'X-Parse-REST-API-Key': PARSE_REST_KEY,
+          'Content-Type':'application/json'
+          },
+        data: formData
+      })
       .error(function(){
         console.log('Oops! Something bad happened.');
       });
     },
 
     open: function(objectId){
-      return $http({method: 'GET', url: this.apiPath + '/' + objectId, headers: {'X-Parse-Application-Id': PARSE_APP_ID, 'X-Parse-REST-API-Key': PARSE_REST_KEY, 'Content-Type':'application/json'}});
+      return $http({
+        method: 'GET',
+        url: this.apiPath + '/' + objectId,
+        headers: {
+          'X-Parse-Application-Id': PARSE_APP_ID,
+          'X-Parse-REST-API-Key': PARSE_REST_KEY,
+          'Content-Type':'application/json'
+        }
+      });
     },
 
+
     list: function(){
-      return $http({method: 'GET', url: this.apiPath, headers: {'X-Parse-Application-Id': PARSE_APP_ID, 'X-Parse-REST-API-Key': PARSE_REST_KEY, 'Content-Type':'application/json'}});
+      return $http({
+        method: 'GET',
+        url: this.apiPath,
+        headers: {
+          'X-Parse-Application-Id': PARSE_APP_ID,
+          'X-Parse-REST-API-Key': PARSE_REST_KEY,
+          'Content-Type':'application/json'
+        }
+      });
     },
 
     update: function(objectId, formData){
-      return $http({method: 'PUT', url: this.apiPath + '/' + objectId, headers: {'X-Parse-Application-Id': PARSE_APP_ID, 'X-Parse-REST-API-Key': PARSE_REST_KEY, 'Content-Type':'application/json'}, data: formData})
+      return $http({
+        method: 'PUT',
+        url: this.apiPath + '/' + objectId,
+        headers: {
+          'X-Parse-Application-Id': PARSE_APP_ID,
+          'X-Parse-REST-API-Key': PARSE_REST_KEY,
+          'Content-Type':'application/json'
+        },
+        data: formData
+      })
       .error(function(){
         console.log('Oops! Something bad happened.');
       });
@@ -28,6 +63,15 @@ app.factory('crfData', function($http, $location){
 
     delete: function(objectId){
       console.log('crfData.delete hit');
-      return $http({method: 'DELETE', url: this.apiPath + '/' + objectId, headers: {'X-Parse-Application-Id': PARSE_APP_ID, 'X-Parse-REST-API-Key': PARSE_REST_KEY, 'Content-Type':'application/json'}});    }
+      return $http({
+        method: 'DELETE',
+        url: this.apiPath + '/' + objectId,
+        headers: {
+          'X-Parse-Application-Id': PARSE_APP_ID,
+          'X-Parse-REST-API-Key': PARSE_REST_KEY,
+          'Content-Type':'application/json'
+        }
+      });
+    }
   };
 });
--- app/scripts/services/users.js
+++ app/scripts/services/users.js
@@ -6,10 +6,28 @@ app.factory('Users', function($http){
 
     create: function(user){
       console.log('users.create hit with user: ' + user.username);
+      return $http({
+        method: 'POST',
+        url: this.apiPath,
+        headers: {
+          'X-Parse-Application-Id': PARSE_APP_ID,
+          'X-Parse-REST-API-Key': PARSE_REST_KEY,
+          'Content-Type':'application/json'
+          },
+        data: user
+      });
     },
 
-    login: function(){
-      return $http({method: 'GET', url: 'https://api.parse.com/1/login', headers: {'X-Parse-Application-Id': PARSE_APP_ID, 'X-Parse-REST-API-Key': PARSE_REST_KEY, 'Content-Type':'application/json'}});
+    login: function(user){
+      return $http({
+        method: 'GET',
+        url: 'https://api.parse.com/1/login',
+        headers: {
+          'X-Parse-Application-Id': PARSE_APP_ID,
+          'X-Parse-REST-API-Key': PARSE_REST_KEY,
+          'Content-Type':'application/json'
+          }
+        });
     }
   };
 });
