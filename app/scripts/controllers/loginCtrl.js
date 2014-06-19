623154939b666d66a38eeaf93f105d8b893dbf1e
allenjprice <allenjprice@gmail.com>
2014-06-19 10:45:59 -0500

    Still tinkering with auth.

--- app/scripts/controllers/loginCtrl.js
+++ app/scripts/controllers/loginCtrl.js
@@ -11,8 +11,6 @@ app.controller('loginCtrl', function($scope, AuthService){
     .success(function(data){
       console.log('logged as user: ' + data.username);
       console.log('sessionToken: '+ data.sessionToken);
-      $scope.currentUser = data;
-      $scope.loggedIn = true;
     })
     .error(function(data){
       console.log('there was a problem and you are not logged in.');
