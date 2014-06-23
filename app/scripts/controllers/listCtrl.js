f3ac60f6b5faa0f8bf19c9330499826784681775
allenjprice <allenjprice@gmail.com>
2014-06-23 15:13:05 -0500

    Moved login functionality to the list view, since that's the only view that needs it right now.

--- app/scripts/controllers/listCtrl.js
+++ app/scripts/controllers/listCtrl.js
@@ -1,6 +1,20 @@
 'use strict';
 
-app.controller('listCtrl', function($scope, crfData){
+app.controller('listCtrl', function($scope, crfData, Users){
+  $scope.currentUser = '';
+  $scope.loggedIn = false;
+  $scope.user = {
+    username: '',
+    password: ''
+  };
+
+  $scope.login = function() {
+    Users.login($scope.user)
+    .success(function(){
+      $scope.loggedIn = true;
+    });
+  };
+
   crfData.list().success(function(data){
     $scope.crfList = data.results;
   });
@@ -9,4 +23,5 @@ app.controller('listCtrl', function($scope, crfData){
     //TODO: force view to update on delete
     return crfData.delete(objectId);
   };
+
 });
